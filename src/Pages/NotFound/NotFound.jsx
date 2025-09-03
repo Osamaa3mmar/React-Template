c0fc5b0eedf74baca767c42c2535025288604557
Osamaa3mmar <osamaammarothman@gmail.com>
2025-09-03 11:44:50 +0300

    add: tailwind css

--- src/Pages/NotFound/NotFound.jsx
+++ src/Pages/NotFound/NotFound.jsx
@@ -7,7 +7,7 @@ const NotFound = () => {
     <div className="notfound-container">
       <div className="notfound-content">
         <i className="pi pi-exclamation-triangle notfound-icon" />
-        <h1>404</h1>
+        <h1 >404</h1>
         <h2>Page Not Found</h2>
         <p>
           Sorry, the page you are looking for does not exist or has been moved.
